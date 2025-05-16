ff3eb0a9aabc690c574a588be2c8e7954797b1a6
krausediego <diegoemanuelk@gmail.com>
2025-05-16 15:12:44 -0300

    feat: Input component.

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+import { TextInput, View, Text, type TextInputProps } from "react-native";
+
+type InputProps = TextInputProps & {
+  IconLeft: React.ElementType;
+  label?: string;
+};
+
+const Input = React.forwardRef<
+  React.ComponentRef<typeof TextInput>,
+  InputProps
+>(({ className, label, IconLeft, onChange, ...props }, ref) => {
+  const [hasValue, setHasValue] = React.useState<string | undefined>(undefined);
+
+  return (
+    <View className="gap-2">
+      {label && <Text className="font-600 text-xl text-grey-900">{label}</Text>}
+      <View className="relative justify-center">
+        <View className="absolute left-5 z-10">
+          <IconLeft />
+        </View>
+        <TextInput
+          ref={ref}
+          className={cn(
+            "h-[65px] w-full rounded-[10px] bg-grey-100 pl-[52px] font-400 text-xl text-grey-900 placeholder:text-grey-500",
+            hasValue && "font-600",
+            className,
+          )}
+          onChangeText={(e) => {
+            setHasValue(e);
+            onChange;
+          }}
+          {...props}
+        />
+      </View>
+    </View>
+  );
+});
+Input.displayName = "Input";
+
+export { Input };
