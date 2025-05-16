b88e6d147aa8ecc32baac090abe6d396eea430d7
krausediego <diegoemanuelk@gmail.com>
2025-05-16 15:08:15 -0300

    feat: Change root App file.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import "./global.css";
 
-import App from './App';
+import App from "./src/App/app";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
